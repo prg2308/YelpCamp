b5b192b8bd2679a6f835b9f76951af718cccb184
Prayag Sharma <sriprayag@gmail.com>
2021-09-21 01:32:25 +0530

    Added campgrounds controller

---
+++ controllers/campground.js
@@ -0,0 +1,62 @@
+const mongoose = require('mongoose')
+
+const ExpressError = require('../utilities/ExpressError')
+const Campground = require('../models/campground')
+
+module.exports.index = async (req, res, next) => {
+    const campgrounds = await Campground.find({})
+    res.render('campgrounds/index.ejs', { campgrounds })
+}
+
+module.exports.new = (req, res) => {
+    if (!req.isAuthenticated()) {
+        req.flash('error', 'You must be logged in!')
+        return res.redirect('/login')
+    }
+    res.render('campgrounds/new')
+}
+
+module.exports.show = async (req, res, next) => {
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+        throw new ExpressError('Page not found', 404)
+    }
+    const campground = await Campground.findById(req.params.id).populate({
+        path: 'reviews',
+        populate: {
+            path: 'author'
+        }
+    }).populate('author')
+    if (!campground) {
+        req.flash('error', 'Cannot find Campground!')
+        return res.redirect('/campgrounds')
+    }
+    res.render('campgrounds/show.ejs', { campground })
+}
+
+module.exports.renderEdit = async (req, res) => {
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+        throw new ExpressError('Page not found', 404)
+    }
+    const campground = await Campground.findById(req.params.id)
+    res.render('campgrounds/edit.ejs', { campground })
+}
+
+module.exports.create = async (req, res) => {
+    const campground = new Campground(req.body)
+    campground.author = req.user._id
+    await campground.save();
+    req.flash('success', 'Created new Campground!')
+    res.redirect(`/campgrounds/${campground._id}`)
+}
+
+module.exports.edit = async (req, res) => {
+    await Campground.findByIdAndUpdate(req.params.id, req.body)
+    req.flash('success', 'Edited Campground!')
+    res.redirect(`/campgrounds/${campground._id}`)
+}
+
+module.exports.delete = async (req, res) => {
+    const campground = await Campground.findByIdAndDelete(req.params.id)
+    req.flash('success', `Deleted ${campground.title}`)
+    res.redirect('/campgrounds')
+}
--- router/campground.js
+++ router/campground.js
@@ -1,67 +1,43 @@
 const express = require('express')
 const router = express.Router()
-const mongoose = require('mongoose')
 
-const ExpressError = require('../utilities/ExpressError')
 const catchAsync = require('../utilities/catchAsync.js')
-const Campground = require('../models/campground')
 const { isLoggedIn, validateCampground, isAuth } = require('../utilities/middleware')
+const campgrounds = require('../controllers/campground')
 
-router.get('/', catchAsync(async (req, res, next) => {
-    const campgrounds = await Campground.find({})
-    res.render('campgrounds/index.ejs', { campgrounds })
-}))
+router.get('/', catchAsync(campgrounds.index))
 
-router.get('/new', isLoggedIn, (req, res) => {
-    if (!req.isAuthenticated()) {
-        req.flash('error', 'You must be logged in!')
-        return res.redirect('/login')
-    }
-    res.render('campgrounds/new')
-})
+router.get('/new', isLoggedIn, campgrounds.new)
 
-router.get('/:id', catchAsync(async (req, res, next) => {
-    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
-        throw new ExpressError('Page not found', 404)
-    }
-    const campground = await Campground.findById(req.params.id).populate({
-        path: 'reviews',
-        populate: {
-            path: 'author'
-        }
-    }).populate('author')
-    if (!campground) {
-        req.flash('error', 'Cannot find Campground!')
-        return res.redirect('/campgrounds')
-    }
-    res.render('campgrounds/show.ejs', { campground })
-}))
+router.get('/:id', catchAsync(campgrounds.show))
 
-router.get('/:id/edit', isLoggedIn, isAuth, catchAsync(async (req, res) => {
-    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
-        throw new ExpressError('Page not found', 404)
-    }
-    res.render('campgrounds/edit.ejs', { campground })
-}))
+router.get(
+    '/:id/edit',
+    isLoggedIn,
+    isAuth,
+    catchAsync(campgrounds.renderEdit)
+)
 
-router.post('/', isLoggedIn, validateCampground, catchAsync(async (req, res) => {
-    const campground = new Campground(req.body)
-    campground.author = req.user._id
-    await campground.save();
-    req.flash('success', 'Created new Campground!')
-    res.redirect(`/campgrounds/${campground._id}`)
-}))
+router.post(
+    '/',
+    isLoggedIn,
+    validateCampground,
+    catchAsync(campgrounds.create)
+)
 
-router.put('/:id', isLoggedIn, isAuth, validateCampground, catchAsync(async (req, res) => {
-    await Campground.findByIdAndUpdate(req.params.id, req.body)
-    req.flash('success', 'Edited Campground!')
-    res.redirect(`/campgrounds/${campground._id}`)
-}))
+router.put(
+    '/:id',
+    isLoggedIn,
+    isAuth,
+    validateCampground,
+    catchAsync(campgrounds.edit)
+)
 
-router.delete('/:id', isLoggedIn, isAuth, catchAsync(async (req, res) => {
-    await Campground.findByIdAndDelete(req.params.id)
-    req.flash('success', `Deleted ${campground.title}`)
-    res.redirect('/campgrounds')
-}))
+router.delete(
+    '/:id',
+    isLoggedIn,
+    isAuth,
+    catchAsync(campgrounds.delete)
+)
 
 module.exports = router
