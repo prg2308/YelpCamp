1ca4c8654b9bb7d9b5e6a380ab5772793cbffbad
Prayag Sharma <sriprayag@gmail.com>
2021-10-01 01:14:37 +0530

    Made minor syling changes ro maps

--- public/js/mapScript.js
+++ public/js/mapScript.js
@@ -1,11 +1,17 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoicHJnMjMwOCIsImEiOiJja3U2dHh5NGwzMWdqMnZxaGs3eGU2MWplIn0.ZNrycoStLr6VIiACqNV0gw';
 const map = new mapboxgl.Map({
-    container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/streets-v11', // style URL
+    container: 'map',
+    style: 'mapbox://styles/mapbox/outdoors-v11',
     center: campground.geometry.coordinates,
     zoom: 12
 });
 
 const marker1 = new mapboxgl.Marker()
     .setLngLat(campground.geometry.coordinates)
+    .setPopup(
+        new mapboxgl.Popup({ offset: 25 })
+            .setHTML(
+                `<h3>${campground.title}</h3><p>${campground.location}</p>`
+            )
+    )
     .addTo(map);
