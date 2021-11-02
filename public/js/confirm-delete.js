d21d8d4c216c4003ef506b547fcbc33465302da9
Prayag Sharma <85249351+prg2308@users.noreply.github.com>
2021-11-02 21:56:45 +0530

    Change to confirm delete script

--- public/js/confirm-delete.js
+++ public/js/confirm-delete.js
@@ -1,7 +1,7 @@
 const buttons = document.querySelectorAll(".btn-danger");
 for (let button of buttons) {
     button.addEventListener('click', function (e) {
-        const res = confirm('Confirm Delete?')
+        const res = confirm('Confirm Delete? You cant undo this action')
         if (!res) {
             e.preventDefault();
         }
