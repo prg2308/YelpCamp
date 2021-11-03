1c79a73c70a5f9a98198e0396a85101198f633d1
Prayag Sharma <85249351+prg2308@users.noreply.github.com>
2021-11-03 17:11:19 +0530

    Config update

---
+++ config/env.js
@@ -0,0 +1,9 @@
+module.exports.cloud = {
+    key: 895825456278268,
+    name: 'dmal3lkc1',
+    secret: '5IuHwmrPDOhMcI50BBwGEe3Qj6E'
+}
+
+module.exports.mapboxToken = 'pk.eyJ1IjoicHJnMjMwOCIsImEiOiJja3U2dHh5NGwzMWdqMnZxaGs3eGU2MWplIn0.ZNrycoStLr6VIiACqNV0gw'
+
+module.exports.dbUrl = 'mongodb+srv://seeder:8600G8fep2308@prg-cluster.3zoss.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
