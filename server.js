9747deda5847bea1b84be71ac29db015a082d77b
Prayag Sharma <85249351+prg2308@users.noreply.github.com>
2021-11-03 11:26:11 +0530

    XSS prevention, HTML sanitization and helmet

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const passport = require('passport')
 const flash = require('connect-flash')
 const LocalStrategy = require('passport-local')
 const ejsMate = require('ejs-mate')
+const mongoSanitize = require('express-mongo-sanitize')
+const helmet = require("helmet");
 
 const campgroundRoutes = require('./router/campground')
 const reviewRoutes = require('./router/review')
@@ -20,11 +22,13 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const sessionConfig = {
+    name: 'session',
     secret: 'key',
     resave: false,
     saveUninitialized: true,
     cookie: {
         httpOnly: true,
+        //secure: true,
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
         maxAge: 1000 * 60 * 60 * 24 * 7
     }
@@ -46,6 +50,55 @@ app.engine('ejs', ejsMate)
 
 app.use(session(sessionConfig))
 app.use(flash())
+app.use(helmet());
+
+//Update scrpts and styles urls with bootstrap updates
+
+const scriptSrcUrls = [
+    "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js",
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://api.mapbox.com/",
+    "https://kit.fontawesome.com/",
+    "https://cdnjs.cloudflare.com/",
+    "https://cdn.jsdelivr.net",
+];
+const styleSrcUrls = [
+    "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css",
+    "https://kit-free.fontawesome.com/",
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.mapbox.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://fonts.googleapis.com/",
+    "https://use.fontawesome.com/",
+];
+const connectSrcUrls = [
+    "https://api.mapbox.com/",
+    "https://a.tiles.mapbox.com/",
+    "https://b.tiles.mapbox.com/",
+    "https://events.mapbox.com/",
+];
+const fontSrcUrls = [];
+app.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: [],
+            connectSrc: ["'self'", ...connectSrcUrls],
+            scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+            styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+            workerSrc: ["'self'", "blob:"],
+            objectSrc: [],
+            imgSrc: [
+                "'self'",
+                "blob:",
+                "data:",
+                "https://res.cloudinary.com/dmal3lkc1/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
+                "https://images.unsplash.com/",
+            ],
+            fontSrc: ["'self'", ...fontSrcUrls],
+        },
+    })
+);
 
 app.use(passport.initialize())
 app.use(passport.session())
@@ -57,6 +110,9 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(methodOverride('_method'));
+app.use(mongoSanitize({
+    replaceWith: '_'
+}))
 app.use((req, res, next) => {
     if (!['/login', '/'].includes(req.originalUrl)) {
         req.session.returnTo = req.originalUrl
